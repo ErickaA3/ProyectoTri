9ae6544699f48a7e6a3d4873386bf486e791b025
Ericka Quirós <erickacloromiro@gmail.com>
2025-08-30 19:06:09 -0600

    resumenes template

--- djangoproject/resumenes/views.py
+++ djangoproject/resumenes/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
-
 def resumenes_home(request):
     context = {
         'titulo': 'Resúmenes',
