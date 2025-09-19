e2df0e894b29f7df82bdc369ce67440ef4ad94c2
Ericka Quirós <erickacloromiro@gmail.com>
2025-09-18 21:18:23 -0600

    flashcards-1

--- djangoproject/flashcards/urls.py
+++ djangoproject/flashcards/urls.py
@@ -4,5 +4,18 @@ from . import views
 app_name = 'flashcards'
 
 urlpatterns = [
+    # Cambiar esta línea
+    path('', views.flashcards_home, name='flashcards_home'),
+    
+    # Por esta
     path('', views.flashcards_home, name='home'),
+    
+    # El resto permanece igual...
+    path('process-file/', views.process_file, name='process_file'),
+    path('process-text/', views.process_text, name='process_text'),
+    path('collection/<uuid:collection_id>/', views.view_collection, name='view_collection'),
+    path('my-flashcards/', views.my_flashcards, name='my_flashcards'),
+    path('study/<uuid:collection_id>/', views.study_collection, name='study_collection'),
+    path('session/<int:session_id>/complete/', views.complete_study_session, name='complete_study_session'),
+    path('collection/<uuid:collection_id>/delete/', views.delete_collection, name='delete_collection'),
 ]
